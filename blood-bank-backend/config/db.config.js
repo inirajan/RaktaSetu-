6aaa4bd026ce8e5563d0c912eb9163010b8a875b
inirajan <nirasrokaya@gmail.com>
2025-07-10 18:12:51 +0545

    db string

--- blood-bank-backend/config/db.config.js
+++ blood-bank-backend/config/db.config.js
@@ -2,7 +2,10 @@ import { connect } from "mongoose";
 
 async function dbConnect() {
   try {
-    const isConnect = await connect(`${process.env.MONGO_URL}blood_Bank`);
+    //local
+    // const isConnect = await connect(`${process.env.MONGO_URL}blood_Bank`);
+    //website
+    const isConnect = await connect(`${process.env.MONGO_URL_WEB}blood_Bank`);
     console.log(`MonogDB is connected.${isConnect.connection.host}`);
   } catch (err) {
     console.log(err.message);
