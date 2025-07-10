2751a407698cc04f972f72f31a6b4d22e106e773
inirajan <nirasrokaya@gmail.com>
2025-07-10 19:01:14 +0545

    second commit

--- blood-bank-backend/app.js
+++ blood-bank-backend/app.js
@@ -10,10 +10,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    // origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
